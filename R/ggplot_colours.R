3590f55bc5f7b1f79b339f138769bf88c5bf1a55
BJ <95271342+DoctorBJones@users.noreply.github.com>
2021-12-24 12:17:56 +1100

    Makes palettes ordered

--- R/ggplot_colours.R
+++ R/ggplot_colours.R
@@ -47,12 +47,71 @@ bj_palettes <- list(
   `orange gradient` = bj_cols("dark orange", "orange 2", "orange 3", "orange 4", "orange 5", "orange 6", "orange 7")
 )
 
+# Create ordered palettes
+colorRamp_d <- function (colors, n,
+                         bias = 1,
+                         space = c("rgb", "Lab"),
+                         interpolate = c("linear", "spline"),
+                         alpha = FALSE) {
+  
+  if (bias <= 0)
+    stop("'bias' must be positive")
+  
+  if (!missing(space) && alpha)
+    stop("'alpha' must be false if 'space' is specified")
+  
+  colors <- t(grDevices::col2rgb(colors, alpha = alpha)/255)
+  space <- match.arg(space)
+  interpolate <- match.arg(interpolate)
+  
+  if (space == "Lab")
+    colors <- grDevices::convertColor(colors, from = "sRGB", to = "Lab")
+  interpolate <- switch(interpolate, linear = stats::approxfun, spline = stats::splinefun)
+  
+  
+  if (n<nrow(colors)) colors <- colors[1:n,]
+  
+  if ((nc <- nrow(colors)) == 1L) {
+    colors <- colors[c(1L, 1L), ]
+    nc <- 2L
+  }
+  
+  x <- seq.int(0,1, length.out = nc)^bias
+  palette <- c(interpolate(x, colors[, 1L]), 
+               interpolate(x, colors[, 2L]), 
+               interpolate(x, colors[, 3L]),
+               if (alpha) interpolate(x, colors[, 4L]))
+  
+  roundcolor <- function(rgb) pmax(pmin(rgb, 1), 0)
+                         if (space == "Lab")
+                           function (x) roundcolor(grDevices::convertColor(cbind(palette[[1L]](x),
+                                                                                 palette[[2L]](x), palette[[3L]](x),
+                                                                                 if (alpha)
+                                                                                   palette[[4L]](x)), from = "Lab",
+                                                                           to = "sRGB")) * 255
+  else function (x) roundcolor(cbind(palette[[1L]](x), palette[[2L]](x),
+                                     palette[[3L]](x), if (alpha)
+                                       palette[[4L]](x))) * 255
+  }
+
+colorRampPalette_d <- function (colors, ...) {
+  function (n) {
+    ramp <- colorRamp_d(colors, n, ...)
+    x <- ramp(seq.int(0, 1, length.out = n))
+    if (ncol(x) == 4L)
+        grDevices::rgb(x[, 1L], x[, 2L], x[, 3L], x[, 4L], maxColorValue = 255)
+        else grDevices::rgb(x[, 1L], x[, 2L], x[, 3L], maxColorValue = 255)
+  }
+}
+
+
+
 bj_pal <- function(palette = "main", reverse = FALSE, ...) {
   pal <- bj_palettes[[palette]]
 
   if (reverse) pal <- rev(pal)
 
-  grDevices::colorRampPalette(pal, ...)
+  colorRampPalette_d(pal, ...)
 }
 
 #' Color scale constructor
