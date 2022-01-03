b46e652343f728a681be0dcd4d4e247e12245a7a
BJ <95271342+DoctorBJones@users.noreply.github.com>
2022-01-04 07:28:10 +1100

    Checks for font before installing

--- R/global.R
+++ R/global.R
@@ -1,5 +1,8 @@
 
-# Import font for package
+# Import font for package if not already istalled
+if (! "Nanum Gothic" %in% sysfonts::font_families_google()) {
+    sysfonts::font_add_google("Nanum Gothic", "Nanum Gothic")
+}
 
-sysfonts::font_add_google("Nanum Gothic", "Nanum Gothic")
+#default to showtext
 showtext::showtext_auto()
